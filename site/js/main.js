22cdc31e3e30e57b73a94ab23ea9c4471af0c8df
Itggot-Victor-Ek <victor.ek@itggot.se>
2017-03-29 21:57:43 +0200

    första sida 99.9% klar, 3 sidor påörjade

--- site/js/main.js
+++ site/js/main.js
@@ -3,4 +3,10 @@ function hamburgar_meny() {
     element = document.querySelector(".hamburger");
     console.log(element);
     element.classList.toggle("hamburgare_js");
+    element = document.querySelector(".moreH1");
+    element.classList.toggle("moreH1_js");
+}
+
+function heart() {
+
 }
